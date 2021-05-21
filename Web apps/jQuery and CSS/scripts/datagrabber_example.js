6beb5f7158f15f385b15d0624de3ce8191c4ea69
adrianwojcikpp <54798218+adrianwojcikpp@users.noreply.github.com>
2021-05-21 12:05:01 +0200

    data grabber cache control

--- Web apps/jQuery and CSS/scripts/datagrabber_example.js
+++ Web apps/jQuery and CSS/scripts/datagrabber_example.js
@@ -19,9 +19,9 @@ const url = 'http://localhost/server mock/chartdata.json'; ///< server app with 
 * @retval random number from range <-1, 1>
 */
 function getRand() {
-	const maxVal =  1.0;
-	const minVal = -1.0;
-	return (maxVal-minVal)*Math.random() + minVal;
+  const maxVal =  1.0;
+  const minVal = -1.0;
+  return (maxVal-minVal)*Math.random() + minVal;
 }
 
 /**
@@ -29,48 +29,48 @@ function getRand() {
 * @param y New y-axis value 
 */
 function addData(y){
-	if(ydata.length > maxSamplesNumber)
-	{
-		removeOldData();
-		lastTimeStamp += sampleTimeSec;
-		xdata.push(lastTimeStamp.toFixed(4));
-	}
-	ydata.push(y);
-	chart.update();
+  if(ydata.length > maxSamplesNumber)
+  {
+    removeOldData();
+    lastTimeStamp += sampleTimeSec;
+    xdata.push(lastTimeStamp.toFixed(4));
+  }
+  ydata.push(y);
+  chart.update();
 }
 
 /**
 * @brief Remove oldest data point.
 */
 function removeOldData(){
-	xdata.splice(0,1);
-	ydata.splice(0,1);
+  xdata.splice(0,1);
+  ydata.splice(0,1);
 }
 
 /**
 * @brief Start request timer
 */
 function startTimer(){
-	timer = setInterval(ajaxJSON, sampleTimeMsec);
+  timer = setInterval(ajaxJSON, sampleTimeMsec);
 }
 
 /**
 * @brief Stop request timer
 */
 function stopTimer(){
-	clearInterval(timer);
+  clearInterval(timer);
 }
 
 /**
 * @brief Send HTTP GET request to IoT server
 */
 function ajaxJSON() {
-	$.ajax(url, {
-		type: 'GET', dataType: 'json',
-		success: function(responseJSON, status, xhr) {
-			addData(+responseJSON.data);
-		}
-	});
+  $.ajax(url, {
+    type: 'GET', dataType: 'json',
+    success: function(responseJSON, status, xhr) {
+      addData(+responseJSON.data);
+    }
+  });
 }
 
 /**
@@ -78,69 +78,68 @@ function ajaxJSON() {
 */
 function chartInit()
 {
-	// array with consecutive integers: <0, maxSamplesNumber-1>
-	xdata = [...Array(maxSamplesNumber).keys()]; 
-	// scaling all values ​​times the sample time 
-	xdata.forEach(function(p, i) {this[i] = (this[i]*sampleTimeSec).toFixed(4);}, xdata);
+  // array with consecutive integers: <0, maxSamplesNumber-1>
+  xdata = [...Array(maxSamplesNumber).keys()]; 
+  // scaling all values ​​times the sample time 
+  xdata.forEach(function(p, i) {this[i] = (this[i]*sampleTimeSec).toFixed(4);}, xdata);
 
-	// last value of 'xdata'
-	lastTimeStamp = +xdata[xdata.length-1]; 
+  // last value of 'xdata'
+  lastTimeStamp = +xdata[xdata.length-1]; 
 
-	// empty array
-	ydata = []; 
+  // empty array
+  ydata = []; 
 
-	// get chart context from 'canvas' element
-	chartContext = $("#chart")[0].getContext('2d');
+  // get chart context from 'canvas' element
+  chartContext = $("#chart")[0].getContext('2d');
 
-	chart = new Chart(chartContext, {
-		// The type of chart: linear plot
-		type: 'line',
+  chart = new Chart(chartContext, {
+    // The type of chart: linear plot
+    type: 'line',
 
-		// Dataset: 'xdata' as labels, 'ydata' as dataset.data
-		data: {
-			labels: xdata,
-			datasets: [{
-				fill: false,
-				label: 'Random timeseries',
-				backgroundColor: 'rgb(255, 0, 0)',
-				borderColor: 'rgb(255, 0, 0)',
-				data: ydata,
-				lineTension: 0
-			}]
-		},
+    // Dataset: 'xdata' as labels, 'ydata' as dataset.data
+    data: {
+      labels: xdata,
+      datasets: [{
+        fill: false,
+        label: 'Random timeseries',
+        backgroundColor: 'rgb(255, 0, 0)',
+        borderColor: 'rgb(255, 0, 0)',
+        data: ydata,
+        lineTension: 0
+      }]
+    },
 
-		// Configuration options
-		options: {
-			responsive: true,
-			maintainAspectRatio: false,
-			animation: false,
-			scales: {
-				yAxes: [{
-					scaleLabel: {
-						display: true,
-						labelString: 'Random value'
-					}
-				}],
-				xAxes: [{
-					scaleLabel: {
-						display: true,
-						labelString: 'Time [s]'
-					}
-				}]
-			}
-		}
-	});
-	
-	ydata = chart.data.datasets[0].data;
-	xdata = chart.data.labels;
-	
-	//$.ajaxSetup({ cache: false });
+    // Configuration options
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      animation: false,
+      scales: {
+        yAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: 'Random value'
+          }
+        }],
+        xAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: 'Time [s]'
+          }
+        }]
+      }
+    }
+  });
+  
+  ydata = chart.data.datasets[0].data;
+  xdata = chart.data.labels;
 }
 
 $(document).ready(() => { 
-	chartInit();
-	$("#start").click(startTimer);
-	$("#stop").click(stopTimer);
-	$("#sampletime").text(sampleTimeMsec.toString());
-	$("#samplenumber").text(maxSamplesNumber.toString());
+  chartInit();
+  $.ajaxSetup({ cache: false }); // Web browser cache control
+  $("#start").click(startTimer);
+  $("#stop").click(stopTimer);
+  $("#sampletime").text(sampleTimeMsec.toString());
+  $("#samplenumber").text(maxSamplesNumber.toString());
 });
